04f6adf3f4e0cc93aeeb2523e4bc7439aad8d8f7
MangelC25 <150280388+MangelC25@users.noreply.github.com>
2024-02-08 20:10:21 -0500

    Commit 2
    Header/ Navbar V2.0

--- JS/Header/Navbar.js
+++ JS/Header/Navbar.js
@@ -1,35 +1,36 @@
 function setupMenu() {
+  const options = document.querySelectorAll(".option");
 
-const options = document.querySelectorAll(".option");
+  options.forEach((option) => {
+    const submenu = option.querySelector("ul");
 
-options.forEach((option) => {
-  const submenu = option.querySelector("ul");
+    option.addEventListener("click", () => {
+      if (submenu) {
+        if (submenu.style.maxHeight === "0px" || !submenu.style.maxHeight) {
+          submenu.style.maxHeight = submenu.scrollHeight + "px";
+        } else {
+          return;
+        }
+      }
 
-  option.addEventListener("click", () => {
+      options.forEach((opt) => {
+        if (opt !== option) {
+          const sub = opt.querySelector("ul");
+          opt.classList.remove("active");
+          if (sub) {
+            sub.style.maxHeight = 0;
+          }
+        }
+      });
+      option.classList.add("active");
+    });
+
     if (submenu) {
-      if (submenu.style.maxHeight === "0px" || !submenu.style.maxHeight) {
-        submenu.style.maxHeight = submenu.scrollHeight + "px";
-      } else {
-        submenu.style.maxHeight = 0;
-      };
-    };
-
-    options.forEach((opt) => {
-      if (opt !== option) {
-        const sub = opt.querySelector("ul");
-        if (sub) {
-          sub.style.maxHeight = 0;
-        };
-      };
-    });
+      submenu.addEventListener("click", (event) => {
+        event.stopPropagation();
+      });
+    }
   });
-
-  if (submenu) {
-    submenu.addEventListener("click", (event) => {
-      event.stopPropagation();
-    });
-  };
-});
-};
+}
 
 export default setupMenu;
